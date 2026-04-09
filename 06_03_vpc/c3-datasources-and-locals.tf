e89a87261fbaf023e893b5970c8588c90a785be0
Dercio Anselmo <dercio@branchdev.io>
2026-04-09 13:14:41 +0400

    Better comments and code cleaned

--- 06_03_vpc/c3-datasources-and-locals.tf
+++ 06_03_vpc/c3-datasources-and-locals.tf
@@ -1,28 +1,11 @@
-#Data Block
+# Datasources
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
-#Locals Block
+# Locals Block
 locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
-  # Making sure the provate and public subnet IPs do not overlaps. using cidrsubnet to calculate the IPs 
-  # Public Subnets:   10.0.0.0/24, 10.0.1.0/24, 10.0.2.0/24
-  public_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
-  # Private Subnets:  10.0.10.0/24, 10.0.11.0/24, 10.0.12.0/24   (shifted by 10)
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
   private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
-
-  #apple@apples-MacBook-Pro 06_Terraform_Basics % terraform console
-  #> cidrsubnet("10.0.0.0/16", 8, 0)
-  #"10.0.0.0/24"
-  #> cidrsubnet("10.0.0.0/16", 8, 1)
-  #"10.0.1.0/24"
-  #> cidrsubnet("10.0.0.0/16", 8, 2)
-  #> cidrsubnet("10.0.0.0/16", 8, 0+10)
-  #"10.0.10.0/24"
-  #> cidrsubnet("10.0.0.0/16", 8, 1+10)
-  #"10.0.11.0/24"
-  #> cidrsubnet("10.0.0.0/16", 8, 2+10)
-  #"10.0.12.0/24"
-  #>  
 }
--- 06_03_vpc/c4-vpc.tf
+++ 06_03_vpc/c4-vpc.tf
@@ -1,90 +1,85 @@
-# Resources
-# VPC
+# Resource-1: VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = merge(var.tags, { Name = "${var.environment_name}-vpc"})
+  enable_dns_support   = true
+  tags = merge(var.tags, { Name = "${var.environment_name}-vpc" })
   lifecycle {
-    prevent_destroy = false # In production must be true.
+    prevent_destroy = false
   }
 }
 
-# Internet Gateway
+# Resource-2: Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-    tags = merge(var.tags, { Name = "${var.environment_name}-igw"})
+  tags = merge(var.tags, { Name = "${var.environment_name}-igw" })
 }
-# Public Subnets (3 Subnets to be generated)
+
+# Resource-3: Public Subnets
 resource "aws_subnet" "public" {
-  for_each = { for idx, az in local.azs : az => local.public_subnets[idx] }  
-  vpc_id = aws_vpc.main.id
-  cidr_block = each.value
-  availability_zone = each.key
+  for_each = { for idx, az in local.azs : az => local.public_subnets[idx] }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = each.key
   map_public_ip_on_launch = true
-  tags = merge(var.tags, { Name = "${var.environment_name}-public-${each.key}"})
+
+  tags = merge(var.tags, {
+    Name = "${var.environment_name}-public-${each.key}"
+  })
 }
 
-# Private Subnets (3 Subnets to be generated)
+# Resource-4: Private Subnets
 resource "aws_subnet" "private" {
-  for_each = { for idx, az in local.azs : az => local.private_subnets[idx] }  
-  vpc_id = aws_vpc.main.id
-  cidr_block = each.value
+  for_each = { for idx, az in local.azs : az => local.private_subnets[idx] }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
   availability_zone = each.key
-  tags = merge(var.tags, { Name = "${var.environment_name}-private-${each.key}"})
+  tags = merge(var.tags, {
+    Name = "${var.environment_name}-private-${each.key}"
+  })
 }
 
-# Elastic IP for NAT Gateway
+# Resource-5: Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {
-    tags = merge(var.tags, { Name = "${var.environment_name}-nat-eip"})
+  tags = merge(var.tags, { Name = "${var.environment_name}-nat-eip" })
 }
 
-# NAT Gateway
+# Resource-6: NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = values(aws_subnet.public)[0].id 
-    # > values({"us-east-1a"="10.0.0.0/24", "us-east-1b"="10.0.1.0/24", "us-east-1c"="10.0.2.0/24"})
-    # [
-    #   "10.0.0.0/24",
-    #   "10.0.1.0/24",
-    #   "10.0.2.0/24",
-    # ]
-    # >  
-  tags = merge(var.tags, { Name = "${var.environment_name}-nat"})
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
+  subnet_id     = values(aws_subnet.public)[0].id
+  tags = merge(var.tags, { Name = "${var.environment_name}-nat" })
   depends_on = [aws_internet_gateway.igw]
 }
 
-# Public Route Table
+# Resource-7: Public Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = merge(var.tags, { Name = "${var.environment_name}-public-rt"})
+  tags = merge(var.tags, { Name = "${var.environment_name}-public-rt" })
 }
 
-# Public Route Table Associated to Public Subnet
+# Resource-8: Public Route Table Associate to Public Subnet
 resource "aws_route_table_association" "public_rt_assoc" {
   for_each = aws_subnet.public
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Private Route Table
+# Resource-9: Private Route Table
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id # Where private subnet is allowed to go to internet
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
-  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt"})
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt" })
 }
 
-# Private Route Table Associated to Private Subnet
+# Resource-10: Private Route Table Association to Private Subnet
 resource "aws_route_table_association" "private_rt_assoc" {
   for_each = aws_subnet.private
   subnet_id      = each.value.id
