956803fd1d7c3f670685625e77e28c7eb9f4fea8
Dercio Anselmo <dercio@branchdev.io>
2026-04-09 12:29:18 +0400

    Added Public and Private Route Table and it's association to Public and Private Subnet

--- 06_03_vpc/c4-vpc.tf
+++ 06_03_vpc/c4-vpc.tf
@@ -58,9 +58,35 @@ resource "aws_nat_gateway" "nat" {
 }
 
 # Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "10.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-public-rt"})
+}
 
 # Public Route Table Associated to Oublic Subnet
+resource "aws_route_table_association" "public_rt_assoc" {
+  for_each = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
 # Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "10.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id # Where private subnet is allowed to go to internet
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt"})
+}
 
 # Private Route Table Associated to Private Subnet
+resource "aws_route_table_association" "private_rt_assoc" {
+  for_each = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
+}
