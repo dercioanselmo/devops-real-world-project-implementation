ac13ab307a6c083a9c4a380dcdb1a815d09ae361
Dercio Anselmo <dercio@branchdev.io>
2026-04-09 04:25:09 +0400

    Added meta-argument lifecyle to add the prevent_destroy, Added the Internet Gateway Added the Public and Private Subnets created using Meta-argument foreach to allow multiple resource in one block.

--- 06_03_vpc/c4-vpc.tf
+++ 06_03_vpc/c4-vpc.tf
@@ -5,13 +5,34 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = merge(var.tags, { Name = "${var.environment_name}-vpc"})
+  lifecycle {
+    prevent_destroy = false # In production must be true.
+  }
 }
 
 # Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+    tags = merge(var.tags, { Name = "${var.environment_name}-igw"})
+}
+# Public Subnets (3 Subnets to be generated)
+resource "aws_subnet" "public" {
+  for_each = { for idx, az in local.azs : az => local.public_subnets[idx] }  
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+  map_public_ip_on_launch = true
+  tags = merge(var.tags, { Name = "${var.environment_name}-public-${each.key}"})
+}
 
-# Public Subnets
-
-# Private Subnets
+# Private Subnets (3 Subnets to be generated)
+resource "aws_subnet" "private" {
+  for_each = { for idx, az in local.azs : az => local.private_subnets[idx] }  
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-${each.key}"})
+}
 
 # Elastic IP for NAT Gateway
 
