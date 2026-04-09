2e0e32a32f70650252425b32997653f7bf4af6f1
Dercio Anselmo <dercio@branchdev.io>
2026-04-09 13:08:59 +0400

    Route Tables cidr_block bug fix

--- 06_03_vpc/c4-vpc.tf
+++ 06_03_vpc/c4-vpc.tf
@@ -61,7 +61,7 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "10.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = merge(var.tags, { Name = "${var.environment_name}-public-rt"})
@@ -78,7 +78,7 @@ resource "aws_route_table_association" "public_rt_assoc" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "10.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id # Where private subnet is allowed to go to internet
   }
   tags = merge(var.tags, { Name = "${var.environment_name}-private-rt"})
