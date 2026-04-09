24e9cae7590ee7c4c647192dcfac6d0683ad48bd
Dercio Anselmo <dercio@branchdev.io>
2026-04-10 03:14:53 +0400

    IAM role for EKS control plane

---
+++ 07_Terraform_EKS_Cluster/02_EKS/c6_eks_cluster_iamrole.tf
@@ -0,0 +1,29 @@
+# Giving AWS Control Plane access our AWS accunt resources
+# IAM Role 
+# This role is assumed by EKS service to manage the control plane resouces
+resource "aws_iam_role" "eks_cluster" {
+  #Unique name dr the control plane IAM Role
+  name = "${local.name}-eks-cluster-role"
+
+  #Trust policy to allow EKS to assume this role
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17",
+    Statement = [{
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          service = "eks.amazonaws.com"  # Only this service can assume this role
+        }
+    }]
+  })
+
+  tags = var.tags
+}
+
+# Attach the required policy for EKS to manage cluster control plane
+# This is mandatory for all EKS Clusters
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
