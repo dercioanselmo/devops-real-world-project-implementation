2dd555d836ba72170d8b72508924a01ee1c8183f
Dercio Anselmo <dercio@branchdev.io>
2026-04-10 02:31:48 +0400

    Adding the output for the remote data source

--- 07_Terraform_EKS_Cluster/02_EKS/c3_remote-state.tf
+++ 07_Terraform_EKS_Cluster/02_EKS/c3_remote-state.tf
@@ -11,3 +11,14 @@ data "terraform_remote_state" "vpc" {
 }
 
 #Outputs
+output "vpc_id" {
+  value = data.terraform_remote_state.vpc.outputs.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+}
