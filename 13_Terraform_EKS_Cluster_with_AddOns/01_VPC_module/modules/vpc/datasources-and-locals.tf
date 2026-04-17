c06ea90f8737dfbc1e794cffb13029b04c049860
Dercio Anselmo <dercio@branchdev.io>
2026-04-18 02:21:30 +0400

    vpc - Restore to the initial generic algorithm for azs

--- 13_Terraform_EKS_Cluster_with_AddOns/01_VPC_module/modules/vpc/datasources-and-locals.tf
+++ 13_Terraform_EKS_Cluster_with_AddOns/01_VPC_module/modules/vpc/datasources-and-locals.tf
@@ -1,21 +1,11 @@
-# datasources-and-locals.tf
-
+# Datasources
 data "aws_availability_zones" "available" {
   state = "available"
 }
-# datasources-and-locals.tf - Stable version for me-central-1
 
+# Locals Block
 locals {
-  # Explicitly define only stable AZs for me-central-1
-  # Avoid mec1-az2 (me-central-1b) as it is currently unstable
-  azs = ["eu-central-1a", "eu-central-1b"]
-  #TODO: Restore to the initial generic algorithm
-
-  public_subnets = [
-    for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)
-  ]
-
-  private_subnets = [
-    for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)
-  ]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
+  private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
 }
