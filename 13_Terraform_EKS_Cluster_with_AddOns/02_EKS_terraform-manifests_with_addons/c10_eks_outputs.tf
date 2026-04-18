a17d4de67c57c7249c559215f9a8a9e69e26acc3
Dercio Anselmo <dercio@branchdev.io>
2026-04-18 04:20:21 +0400

    AWS Resources added to the cluster: Load Balancer Controller (LBC),  Amazon EBS CSI Driver, Secrets Store CSI Driver (with ASCP), EKS Pod Identity Agent

--- 13_Terraform_EKS_Cluster_with_AddOns/02_EKS_terraform-manifests_with_addons/c10_eks_outputs.tf
+++ 13_Terraform_EKS_Cluster_with_AddOns/02_EKS_terraform-manifests_with_addons/c10_eks_outputs.tf
@@ -1,50 +1,83 @@
-# Output of EKS Cluster API server endpoint
+# ------------------------------------------------------------------------------
+# Output the EKS Cluster API server endpoint
 # Used by kubectl and external tools to communicate with the cluster
+# ------------------------------------------------------------------------------
 output "eks_cluster_endpoint" {
-    value = aws_eks_cluster.main.endpoint
-    description = "EKS API Server endpoint"
+  value       = aws_eks_cluster.main.endpoint
+  description = "EKS API server endpoint"
 }
 
+# ------------------------------------------------------------------------------
+# Output the EKS Cluster ID
+# Used in AWS CLI commands and automation scripts to reference the EKS cluster
+# ------------------------------------------------------------------------------
 output "eks_cluster_id" {
-  value = aws_eks_cluster.main.id
-  description = "The name/id of the EKS cluster"
+  description = "The name/id of the EKS cluster."
+  value       = aws_eks_cluster.main.id
 }
 
-# Kubernetes version used in the EKS cluster
+# ------------------------------------------------------------------------------
+# Output the EKS Cluster Version
+# Helpful for students to use this version in other EKS projects 
+# to find supported EKS Addons based on EKS cluster version
+# ------------------------------------------------------------------------------
 output "eks_cluster_version" {
-  value = aws_eks_cluster.main.version
-  description = "EKS Kubernetes versionr"
+  description = "EKS Kubernetes version"
+  value       = aws_eks_cluster.main.version
 }
 
-# EKS Cluster name. Helpfull when scripting, `aws eks update-kubeconfig`, etc
+# ------------------------------------------------------------------------------
+# Output the name of the EKS cluster
+# Helpful for scripting, `aws eks update-kubeconfig`, etc.
+# ------------------------------------------------------------------------------
 output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
-  description = "EKS Cluster name"
+  value       = aws_eks_cluster.main.name
+  description = "EKS cluster name"
 }
 
-# EKS Cluster certificate authority data. 
-# Necessary when setting up kubeconfig or accessing EKS via API
+
+# ------------------------------------------------------------------------------
+# Output the EKS Cluster Certificate Authority data
+# Needed when setting up kubeconfig or accessing EKS via API
+# ------------------------------------------------------------------------------
 output "eks_cluster_certificate_authority_data" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
+  value       = aws_eks_cluster.main.certificate_authority[0].data
   description = "Base64 encoded CA certificate for kubectl config"
 }
 
-#Pribate Node Group name, useful for autoscaler configs, dashboards, tagging, etc
+# ------------------------------------------------------------------------------
+# Output the logical name of the private node group
+# Useful for autoscaler configs, dashboards, tagging
+# ------------------------------------------------------------------------------
 output "private_node_group_name" {
-  value = aws_eks_cluster.main.id
+  value       = aws_eks_node_group.private_nodes.node_group_name
   description = "Name of the EKS private node group"
 }
 
-# IAM Role ARN used by the EKS Node Group
-# Useful for IRSA setup or attaching additional permission
+# ------------------------------------------------------------------------------
+# Output the IAM Role ARN used by the EKS Node Group
+# Useful for IRSA setup or attaching additional permissions
+# ------------------------------------------------------------------------------
 output "eks_node_instance_role_arn" {
-  value = aws_iam_role.eks_nodegroup_role.arn
+  value       = aws_iam_role.eks_nodegroup_role.arn
   description = "IAM Role ARN used by EKS node group (EC2 worker nodes)"
 }
 
-# Output command to configure kubectl for EKS cluster
-# Can be runned directly after apply, to configure kubectl client on local machine to access the cluster
+# ------------------------------------------------------------------------------
+# Output command to configure kubectl for this EKS cluster
+# Helpful for students to run directly after apply
+# ------------------------------------------------------------------------------
 output "to_configure_kubectl" {
-  value = "aws eks --region ${var.aws_region} update-kubeconfig --name ${local.eks_cluster_name}"
   description = "Command to update local kubeconfig to connect to the EKS cluster"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${local.eks_cluster_name}"
+}
+
+
+# ------------------------------------------------------------------------------
+# Output the EKS Cluster Security Group ID
+# Used by downstream services like RDS, ElastiCache, etc.
+# ------------------------------------------------------------------------------
+output "eks_cluster_security_group_id" {
+  description = "Security group associated with the EKS cluster (used for control plane to node communication)"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
