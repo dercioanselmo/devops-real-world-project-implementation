fb399dd496414a10920477f2a32f89bbc92275b3
Dercio Anselmo <dercio@branchdev.io>
2026-04-29 05:19:17 +0400

    Observability with OpenTelemetryv - Initial Project setup with autoscaling with karpenter and HPA

---
+++ 20_Observability_OpenTelemetry/20_01_EKS_Environment_with_ADOT/01_EKS_Cluster_Environment/03_KARPENTER_terraform-manifests/c1_versions.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.20"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      #version = ">= 3.0"
+      version = "~> 3.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.28"
+    }
+  }
+
+
+  # Remote backend configuration using S3 
+  backend "s3" {
+    bucket         = "tfstate-dev-eu-central-1-a65beq"       
+    key            = "karpenter/dev/terraform.tfstate"            
+    region         = "eu-central-1"                            
+    encrypt        = true                                   
+    use_lockfile   = true     
+  }
+}
+
+provider "aws" {
+  # AWS region to use for all resources (from variables)
+  region = var.aws_region
+}
+
+# Required for public ECR authorization token
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
